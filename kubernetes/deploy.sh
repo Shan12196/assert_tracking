4d0484573344fa180548401dce5c95182d964b5a
Shan12196 <42571994+Shan12196@users.noreply.github.com>
2019-09-11 13:19:57 +0530

    Update deploy.sh

--- kubernetes/deploy.sh
+++ kubernetes/deploy.sh
@@ -1,35 +1,35 @@
-kubectl apply -f kubernetes/persistent-volume.yml
-kubectl apply -f kubernetes/persistent-volume-claim.yml
+kubectl apply -f /opt/kubedeploy/kubernetes/persistent-volume.yml
+kubectl apply -f /opt/kubedeploy/kubernetes/persistent-volume-claim.yml
 
 
 echo "Creating the database credentials..."
 
-kubectl apply -f kubernetes/secret.yml
+kubectl apply -f /opt/kubedeploy/kubernetes/secret.yml
 
 
 echo "Creating the postgres deployment and service..."
 
-kubectl create -f kubernetes/postgres-deployment.yml
-kubectl create -f kubernetes/postgres-service.yml
+kubectl create -f /opt/kubedeploy/kubernetes/postgres-deployment.yml
+kubectl create -f /opt/kubedeploy/kubernetes/postgres-service.yml
 
 
 
 echo "Creating the flask deployment and service..."
 
-kubectl create -f kubernetes/flask-deployment.yml
-kubectl create -f kubernetes/flask-service.yml
+kubectl create -f /opt/kubedeploy/kubernetes/flask-deployment.yml
+kubectl create -f /opt/kubedeploy/kubernetes/flask-service.yml
 
 
-kubectl create -f kubernetes/ui-deployment.yml
-kubectl create -f kubernetes/ui-service.yml
-kubectl create -f kubernetes/devicemanagement-deployment.yml
-kubectl create -f kubernetes/devicemanagement-service.yml
-kubectl create -f kubernetes/usermanagement-deployment.yml
-kubectl create -f kubernetes/usermanagement-service.yml
+kubectl create -f /opt/kubedeploy/kubernetes/ui-deployment.yml
+kubectl create -f /opt/kubedeploy/kubernetes/ui-service.yml
+kubectl create -f /opt/kubedeploy/kubernetes/devicemanagement-deployment.yml
+kubectl create -f /opt/kubedeploy/kubernetes/devicemanagement-service.yml
+kubectl create -f /opt/kubedeploy/kubernetes/usermanagement-deployment.yml
+kubectl create -f /opt/kubedeploy/kubernetes/usermanagement-service.yml
 
 
 echo "Adding the ingress..."
 
-minikube addons enable ingress
-kubectl apply -f kubernetes/minikube-ingress.yml
+#minikube addons enable ingress
+#kubectl apply -f /opt/kubedeploy/kubernetes/minikube-ingress.yml
 
